88f94392e2fad86ec2cf1ad7c094e6841200a2f5
AndyConstantinMomita <57625842+AndyConstantinMomita@users.noreply.github.com>
2020-03-27 17:16:10 +0200

    fixes

--- Lab6/src/ControlPanel.java
+++ Lab6/src/ControlPanel.java
@@ -2,20 +2,22 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class ControlPanel extends JPanel {
     final MainFrame frame;
     JButton saveButtton = new JButton("Save");
-    JButton loadButton = new JButton ("Load");
-    JButton resetButton = new JButton ("Reset");
-    JButton exitButton = new JButton ("exit");
+    JButton loadButton = new JButton("Load");
+    JButton resetButton = new JButton("Reset");
+    JButton exitButton = new JButton("exit");
 
     public ControlPanel(MainFrame frame) {
         this.frame = frame;
         init();
     }
+
     private void init() {
         setLayout(new GridLayout(1, 4));
         saveButtton.addActionListener(this::save);
@@ -28,19 +30,25 @@ public class ControlPanel extends JPanel {
         add(resetButton);
         add(exitButton);
     }
+
     private void save(ActionEvent e) {
         try {
             ImageIO.write(frame.canvas.image, "PNG", new File("c:\\test.png"));
-        } catch (IOException ex) { System.err.println(ex); }
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
     }
 
-    private void load(ActionEvent e){
-        try{
-            ImageIO.read(new File("C:\\Users\\Momo\\Desktop\\cat.jpg"));
-        }catch (IOException ex) { System.err.println(ex); }
+    private void load(ActionEvent e) {
+        try {
+            frame.canvas.image = ImageIO.read(new File("c:\\test.png"));
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+
     }
 
-    private void reset(ActionEvent e){
+    private void reset(ActionEvent e) {
         frame.canvas.graphics.setPaint(Color.white);
         frame.canvas.graphics.fillRect(0, 0, 800, 600);
         frame.canvas.graphics.setPaint(Color.black);
--- Lab6/src/DrawingPanel.java
+++ Lab6/src/DrawingPanel.java
@@ -40,7 +40,13 @@ public class DrawingPanel extends JPanel {
         String color = (String)frame.configPanel.colorCombo.getSelectedItem();
         switch(color){
             case "Black": frame.canvas.graphics.setColor(Color.BLACK);
+            {
+                break;
+            }
             case "Random": frame.canvas.graphics.setColor(new Color(random.nextFloat(),random.nextFloat(),random.nextFloat(),.5f));
+            {
+                break;
+            }
         }
         graphics.fill(new RegularPolygon(x, y, radius, sides));
     }
